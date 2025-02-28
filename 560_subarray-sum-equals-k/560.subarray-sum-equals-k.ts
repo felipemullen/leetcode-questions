bf5c360386fa2b6f61916be132f51e5165c49caf
Felipe Mullen <felipedmullen@gmail.com>
2025-02-27 19:42:50 -0800

    Add skeleton for problem 560

---
+++ 560_subarray-sum-equals-k/560.subarray-sum-equals-k.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=560 lang=typescript
+ *
+ * [560] Subarray Sum Equals K
+ */
+
+// @lc code=start
+function subarraySum(nums: number[], k: number): number {
+    
+};
+// @lc code=end
+
+const nums = [1, 1, 1];
+const k = 2;
+const output = subarraySum(nums, k);
+console.log(output); // expect 2
